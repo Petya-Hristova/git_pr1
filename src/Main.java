e02f2c29b773e65b4014c701b57fb65f6efcb413
Mrs Hristova <petiahristova@abv.bg>
2023-06-14 01:50:11 +0300

    add Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Petya! How are you");
+        System.out.println("Hello Petya! How are you?");
     }
 }
